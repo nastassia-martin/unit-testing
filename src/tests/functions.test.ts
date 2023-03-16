143fa6f633bd1010f0ffef98ca6a0457e759f23a
nastassia martin <nastassia.martin@googlemail.com>
2023-03-16 19:45:09 +0100

    added test to prevent empty title and title less than 3 chars

--- src/tests/functions.test.ts
+++ src/tests/functions.test.ts
@@ -1,32 +1,42 @@
 import { describe, it, expect } from 'vitest'
 import { addTodo } from '../functions'
+import { Todo } from '../types/Todo'
+// import { Result } from '../types/Result'
 
 
 // beforeEach test, empty todo array
+const todo: Todo = {
+    id: 1,
+    title: 'say hello to everyone',
+    completed: false,
+}
+
 
 describe('adds a Todo', () => {
+    const result = addTodo(todo.title, [todo])
+    it.todo('should add a todo to array Todo', () => {
+
+    })
+    // x 3 tests
+
+    // 1 - need to make sure that the id doesn't already exist & create new id
+    // 2 - success must be true
+    // 3 - completed must be false?
+    // 4 - must have title
 
 
+    it('should not add a todo with empty title', () => {
+        expect(todo.title.length).not.toBe(0)
+        expect(result.success).toBe(true)
+        expect(result.error).toBeUndefined()
+    })
 
-    //should add a todo to array Todo x 3 tests
+    it('should not add a todo with title shorter than 3 characters', () => {
+        expect(todo.title.length).toBeGreaterThanOrEqual(3)
+        expect(result.success).toBe(true)
+        expect(result.error).toBeUndefined()
+    })
 
-    // 1 - id to not exist/ be unique
-    // 2 - id + 1?
-    // 3 - 
-
-
-    //should not add a todo with empty title x 3 tests
-
-    // 1 - expect(title.length).not.toHaveLength(0)
-    // 2 - expect (success).toBe(true)
-    // 3 - expect (error).toHaveLength(0)
-
-
-    //should not add a todo with title shorter than 3 characters x 3 tests
-
-    // 1 - expect(title.length)tobeGreaterThan(3)
-    // 2 - expect(success).toBe(true)
-    // 3 - expect(error).toHaveLength(0)
 })
 
 // describe('toggle todo', () => {
