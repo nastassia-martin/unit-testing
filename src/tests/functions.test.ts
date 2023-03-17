59ec9ef09729655a302a4f639b85d984b499692d
nastassia martin <nastassia.martin@googlemail.com>
2023-03-17 19:36:44 +0100

    tests for AddTdo function complete

--- src/tests/functions.test.ts
+++ src/tests/functions.test.ts
@@ -1,51 +1,52 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, afterEach } from 'vitest'
 import { addTodo } from '../functions'
 import { Todo } from '../types/Todo'
-// import { Result } from '../types/Result'
 
 
-// beforeEach test, empty todo array
-const todo: Todo = {
-    id: 1,
-    title: 'say hello to everyone',
-    completed: false,
-}
+let todos: Todo[] = []
 
-
-describe('adds a Todo', () => {
-    const result = addTodo(todo.title, [todo])
-    it.todo('should add a todo to array Todo', () => {
-
+describe('add a Todo', () => {
+    afterEach(() => {
+        todos = []
     })
-    // x 3 tests
 
-    // 1 - need to make sure that the id doesn't already exist & create new id
-    // 2 - success must be true
-    // 3 - completed must be false?
-    // 4 - must have title
+    it('should add a todo to array Todo', () => {
+        const title = "say hello to everyone"
+        const result = addTodo(title, todos)
 
+        expect(todos.length).toBeGreaterThanOrEqual(1)
+        expect(result.success).toBe(true)
+        expect(title).toBe(todos[0].title)
+    })
 
     it('should not add a todo with empty title', () => {
-        expect(todo.title.length).not.toBe(0)
-        expect(result.success).toBe(true)
-        expect(result.error).toBeUndefined()
+        const title = ""
+        const result = addTodo(title, todos)
+
+        expect(title).toBe("")
+        expect(result.success).toBe(false)
+        expect(result.error).toContain('Title cannot be empty')
     })
 
     it('should not add a todo with title shorter than 3 characters', () => {
-        expect(todo.title.length).toBeGreaterThanOrEqual(3)
-        expect(result.success).toBe(true)
-        expect(result.error).toBeUndefined()
+        const title = "IG"
+        const result = addTodo(title, todos)
+
+        expect(title.length).toBeLessThanOrEqual(3)
+        expect(result.success).toBe(false)
+        expect(result.error).toContain('Title must be at least 3 characters long')
     })
 
 })
 
-// describe('toggle todo', () => {
-//     //should toggle a todo x 2 tests
-//     //should not toggle a todo that does not exist x 2 tests
-// 1 - expect (COMPLETED).toHaveReturned() - true
-// })
 
-// describe('delete todo', () => {
-//     //should delete a todo x 2 tests
-//     //should not delete a todo that does not exist x 2 tests
-// })
+//describe('toggle todo', () => {
+    //should toggle a todo x 2 tests
+    //should not toggle a todo that does not exist x 2 tests
+
+//})
+
+//describe('delete todo', () => {
+    //should delete a todo x 2 tests
+    //should not delete a todo that does not exist x 2 tests
+//})
