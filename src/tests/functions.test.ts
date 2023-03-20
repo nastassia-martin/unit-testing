e4eec9fe7165acfc282706ae1a86d45156b44ba4
nastassia martin <nastassia.martin@googlemail.com>
2023-03-20 09:38:08 +0100

    added more tests to AddTodo and tidied up code

--- src/tests/functions.test.ts
+++ src/tests/functions.test.ts
@@ -26,6 +26,7 @@ describe('add a Todo', () => {
         expect(title).toBe("")
         expect(result.success).toBe(false)
         expect(result.error).toContain('Title cannot be empty')
+        expect(todos.length).toBe(0)
     })
 
     it('should not add a todo with title shorter than 3 characters', () => {
@@ -35,8 +36,8 @@ describe('add a Todo', () => {
         expect(title.length).toBeLessThanOrEqual(3)
         expect(result.success).toBe(false)
         expect(result.error).toContain('Title must be at least 3 characters long')
+        expect(todos.length).toBe(0)
     })
-
 })
 
 
@@ -65,13 +66,12 @@ describe('toggle todo', () => {
             { id: 1, title: "understand testing", completed: false },
         ]
 
-        const id = 2 //todos[1].id
+        const id = 2
         const result = toggleTodo(id, todos)
 
         expect(result.success).toBe(false)
         expect(result.error).toContain('Todo not found')
     })
-
 })
 
 describe('delete todo', () => {
@@ -80,9 +80,7 @@ describe('delete todo', () => {
         todos = []
     })
 
-
-
-    it('should delete a todo', () => { //x 2 tests
+    it('should delete a todo', () => {
         todos = [
             { id: 1, title: "understand testing", completed: false },
             { id: 2, title: "make friends with ts", completed: false },
@@ -92,10 +90,9 @@ describe('delete todo', () => {
 
         expect(result.success).toBe(true)
         expect(todos.length).toBe(1)
-
     })
 
-    it('should not delete a todo that does not exist', () => { //x 2 tests
+    it('should not delete a todo that does not exist', () => {
         todos = [
             { id: 1, title: "understand testing", completed: false }
         ]
@@ -105,7 +102,6 @@ describe('delete todo', () => {
 
         expect(result.error).toContain('Todo not found')
         expect(result.success).toBe(false)
-
+        expect(todos.length).toBe(1)
     })
-
 })
